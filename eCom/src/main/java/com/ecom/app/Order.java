549c2a736b10b389c50fa78b22024818010c67ac
Sanket Jain <sanketjaind@gmail.com>
2020-04-19 18:32:23 +0530

    Sanket Jain
    eCommerce App

---
+++ eCom/src/main/java/com/ecom/app/Order.java
@@ -0,0 +1,23 @@
+package com.ecom.app;
+
+public class Order {
+	private int id;
+	private int totalNoOfOrder;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public int getTotalNoOfOrder() {
+		return totalNoOfOrder;
+	}
+	
+	public void setTotalNoOfOrder(int totalNoOfOrder) {
+		this.totalNoOfOrder = totalNoOfOrder;
+	}
+
+}
---
+++ eCom/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into Item values(1, 'laptop', 'dell', 'Electronic', 30000, 5);
+insert into Item values(2, 'earphone', 'realme', 'Electronic', 3000, 2);
+insert into Item values(3, 'shirt', 'Adidas', 'fasion', 3000, 3);
+insert into Item values(4, 'paracetomol', '1mg', 'medicine', 500, 200);
+insert into Item values(5, 'grocery', 'nescafe', 'grocery', 300, 5);
